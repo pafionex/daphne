64ede7e7b501aa5345f926f36f937853fc4001b8
Pavlin Petrov <pavlinpetrov@2create.studio>
2018-12-02 19:07:34 +0200

    Add home, slider, article components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './content/home/home.component';
+import { SliderComponent } from './sections/slider/slider.component';
+import { ArticleComponent } from './sections/article/article.component';
+
+// Font Awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faFacebook);
+library.add(faTwitter);
+library.add(faInstagram);
+library.add(faGooglePlus);
+library.add(faPhone);
+library.add(faMapMarkerAlt);
+library.add(faSearch);
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SliderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
