504c36f932aca274a64f43db8083117ac9ee9e29
Pavlin Petrov <pavlinpetrov@2create.studio>
2018-12-09 10:15:44 +0200

    Add carousell styles and assets

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -13,35 +13,15 @@ export class HomeComponent implements OnInit {
   }
 
   slides = [
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"}
+    {img: './assets/images/slide-1.jpeg'},
+    {img: './assets/images/slide-2.jpeg'},
+    {img: './assets/images/slide-3.jpeg'},
   ];
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
-  
-  addSlide() {
-    this.slides.push({img: "http://placehold.it/350x150/777777"})
-  }
-  
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
-  
-  slickInit(e) {
-    console.log('slick initialized');
-  }
-  
-  breakpoint(e) {
-    console.log('breakpoint');
-  }
-  
-  afterChange(e) {
-    console.log('afterChange');
-  }
-  
-  beforeChange(e) {
-    console.log('beforeChange');
-  }
+  slideConfig = {
+    'slidesToShow': 1,
+    'slidesToScroll': 1,
+    'dots': true,
+    'autoplay': true,
+  };
 
 }
