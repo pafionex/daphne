61637ae7a3b3542872dc0ab86d85db44669221cd
Pavlin Petrov <pavlinpetrov@2create.studio>
2018-12-09 12:22:25 +0200

    Alter article markup, add styles and assets

--- src/app/sections/article/article.component.ts
+++ src/app/sections/article/article.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
+	@Input() title: string;
+	@Input() content: string;
+	@Input() image: string;
 
   constructor() { }
 
